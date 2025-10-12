32be662dd46d5f7439e56098b11493cf62082d1e
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-10-13 00:51:52 +0500

    done

--- server.mjs
+++ server.mjs
@@ -1,7 +1,14 @@
 import app from "./index.mjs"
 import db from "./Config/db.mjs"
-
-
+import Studio from "./Models/Studio.mjs";
+ app.get("/testdb", async (req,res)=>{
+  try {
+    const count = await Studio.countDocuments();
+    res.send({ message: "DB connected", studioCount: count });
+  } catch(err) {
+    res.send({ error: err.message });
+  }
+});
 db.connection.once("open", () => console.log("connected to db"))
   .on("error", (err) => console.log("error connecting db -->", err));
 const PORT = process.env.PORT || 3000;
