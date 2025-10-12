98fb8a76a7c699e1ca065f6347d692e8ccd573bb
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-10-13 02:03:41 +0500

    lastl

--- Config/db.mjs
+++ Config/db.mjs
@@ -1,13 +1,6 @@
-// db.mjs
 import mongoose from 'mongoose';
 import { MongoURL } from './environment.mjs';
 
-const connection = mongoose.createConnection(MongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(MongoURL)
 
-connection.once('open', () => console.log('✅ Connected to DB'))
-          .on('error', (err) => console.log('❌ Error connecting DB -->', err));
-
-export default connection;
+export default mongoose;
--- server.mjs
+++ server.mjs
@@ -1,7 +1,38 @@
-import app from "./index.mjs";
-import db from "./Config/db.mjs"; // now db is the connection
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+import db from "./Config/db.mjs"
+
+
+
+import express from 'express';      
+
+
+import cors from "cors"
+import router from "./Routes/Studio.mjs"
+
+
+
+  const app=express()
+
+app.use(cors())
+  app.use(express.json())
+  app.get("/", (req, res) => {
+  res.send("🚀 API is running on Vercel product post without img");
+  
 });
+app.use("/",router)
+
+
+
+db.connection.once("open", () => console.log("connected to db"))
+  .on("error", (err) => console.log("error connecting db -->", err));
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  // Only start server locally
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+  });}
+
+
+  export default app 
