fca52df1437a258549973545a9cc3a46ff34a3d1
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-10-20 03:52:36 +0500

    added studio id

--- Routes/Studio.mjs
+++ Routes/Studio.mjs
@@ -49,7 +49,15 @@ res.status(201).json({message:" studio post completed "})
     }
 })
 
-
+router.get("/:id", async (req,res) => {
+    try {
+        const idStudio= await Studio.findById(req.params.id)
+        console.log(idStudio)
+        res.status(200).json({message:"getting id of product",data:idStudio})
+    } catch (error) {
+        res.status(500).json({message:"erorr in getting id "})
+    }
+})
 // delete api
 
 router.delete("/:id", async (req, res) => {
